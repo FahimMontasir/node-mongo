423c51256bc21351f6618cfb46b05fe71e6d725f
fahim <fahimmontasirshakil@gmail.com>
2021-03-26 17:24:01 +0600

    practice done

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+
+const users = ['asfaq', 'siam', 'riad', 'russel', 'kobir', 'meherab']
 
 app.get('/', (req, res) => {
   res.send("i know how to open node")
 })
 
+app.get('/fruits/banana', (req, res) => {
+  res.send({ fruit: 'banana', price: 100 })
+})
+//dynamic api
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name })
+})
 
-app.listen(3000, () => console.log("listening to port 3000"))
+//post
+app.post('/addUser', (req, res) => {
+  const user = req.body;
+  user.id = 55;
+  res.send(user)
+  console.log(user)
+})
+
+app.listen(4000, () => console.log("listening to port 4000"))
