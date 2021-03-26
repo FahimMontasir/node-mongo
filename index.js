cffade0347105b9f00846453b8b12243d2a2e7b0
fahim <fahimmontasirshakil@gmail.com>
2021-03-26 12:00:53 +0600

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("i know how to open node")
+})
+
+
+app.listen(3000, () => console.log("listening to port 3000"))
